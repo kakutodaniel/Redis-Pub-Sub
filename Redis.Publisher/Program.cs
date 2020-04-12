94e389ed3efb97e66900bdcb15f6fe6d7a6d7f7f
Daniel <kakutodaniel@hotmail.com>
2020-04-12 18:47:25 +0100

    init

---
+++ Redis.Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Redis.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = ConfigurationOptions.Parse("localhost:6379");
+
+            var connection = ConnectionMultiplexer.Connect(configuration);
+
+            var publisher = connection.GetSubscriber();
+
+            const string channelName = "test-channel";
+
+            var db = connection.GetDatabase();
+
+            for (int i = 0; i < 50; i++)
+            {
+               db.ListRightPush(new RedisKey("key1"), new RedisValue($"value-{i}"));
+
+                //publisher.Publish(channelName, $"Message: {channelName} - value-{i}", CommandFlags.FireAndForget);
+                publisher.Publish(channelName, "", CommandFlags.FireAndForget);
+            }
+
+            //publisher.Publish("test-l", $"Publish a message to pattern channel: test-l");
+
+        }
+    }
+}
---
+++ Redis.Subscriber.2/Program.cs
@@ -0,0 +1,77 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Redis.Subscriber._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var configuration = ConfigurationOptions.Parse("localhost:6379");
+
+            var connection = ConnectionMultiplexer.Connect(configuration);
+
+            var subscriber = connection.GetSubscriber();
+
+            const string channelName = "test-channel";
+
+            var db = connection.GetDatabase();
+
+            while (true)
+            {
+                var pop = db.ListLeftPop("key1");
+
+                if (pop.HasValue)
+                {
+
+                    Console.WriteLine(System.Text.Encoding.Default.GetString((byte[])pop.Box()));
+
+                    //Console.WriteLine($"{DateTime.Now.ToString("yyyyMMdd HH:mm:ss.fff")}<Subscriber.1 - Normal - {channel}><{message}>.");
+                }
+                else
+                {
+                    break;
+                }
+
+                Thread.Sleep(4000);
+            }
+
+            //Task.Run(() =>
+            //{
+            //    //var lst = db.ListGetByIndex("key1", 0);
+
+            //    var pop = db.ListLeftPop("key1");
+
+            //    if (pop.HasValue)
+            //    {
+
+            //        Console.WriteLine(System.Text.Encoding.Default.GetString((byte[])pop.Box()));
+
+            //        //Console.WriteLine($"{DateTime.Now.ToString("yyyyMMdd HH:mm:ss.fff")}<Subscriber.1 - Normal - {channel}><{message}>.");
+            //    }
+
+            //});
+
+
+            subscriber.Subscribe(channelName, (channel, message) =>
+            {
+                var pop = db.ListLeftPop("key1");
+
+                if (pop.HasValue)
+                {
+                    Console.WriteLine(System.Text.Encoding.Default.GetString((byte[])pop.Box()));
+                    //Console.WriteLine($"[Subscriber.2] - {message}");
+                }
+
+                //Thread.Sleep(5000);
+                //Console.WriteLine($"{DateTime.Now.ToString("yyyyMMdd HH:mm:ss.fffffff")}<Subscriber.2 - Normal - {channel}><{message}>.");
+            });
+
+            Thread.Sleep(Timeout.Infinite);
+
+        }
+    }
+}
